117e0429ad607df92c9931a93c87c01c7d2b503f
sandipmalii <21121a0567@vidyanikethan.asia>
2024-05-25 17:01:04 +0530

    Added new feature

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>My React Learning with vite</h1>
+      <h2>Sandip Mali</h2>
+      <p>
+        Today, I started learning React from the basics with Hitesh sir. I learned how to create a React app and how to host it.
+      </p>
+    </div>
+  );
+}
+
+export default App
